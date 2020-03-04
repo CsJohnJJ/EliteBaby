63829ba1e604a4184f5d47048112b40fa341d8b4
John Jiang <csjohnjj@gmail.com>
2020-03-04 17:48:39 -0500

    add tile

--- src/tile.js
+++ src/tile.js
@@ -1,19 +1,30 @@
-export default class Tile {
-    constructor(){
+import GameObject from "./game_object";
 
+export default class Tile extends GameObject {
+    constructor(ctx, x, y, vx, vy){
+        super(ctx, x, y, vx, vy);
+        this.width = 50;
+        this.height = 40;
     }
 
     drawTile(){
-        ctx.clearRect(0, 0, 1000, 600);
-        const tileImage = new Image();
-        tileImage.src = "./src/images/tile/cloudtile.png";
-        ctx.drawImage(tileImage, imgPosX, 0, canvasWidth, canvasHeight)
-        imgPosX += imgX
-        if (imgPosX >= canvasWidth) {
-            imgPosX = 1000;
-        } 
-        
+        // ctx.clearRect(0, 0, 1000, 600);
+        // const tileImage = new Image();
+        // tileImage.src = "./src/images/tile/cloudtile.png";
+        // this.ctx.drawImage(tileImage, this.x, this.y, this.width, this.height)
+        // imgPosX += imgX
+        // if (imgPosX >= canvasWidth) {
+        //     imgPosX = 1000;
+        // } 
 
+        //placeholder, square
+        this.ctx.fillStyle = this.isColliding ? '#ff8080' : '#0099b0';
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
     }
 
 
