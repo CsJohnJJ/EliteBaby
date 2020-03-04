44128435a6f1355f51f7e1471ce492969601716c
John Jiang <csjohnjj@gmail.com>
2020-03-04 17:50:06 -0500

    update collision

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles/index.scss";
 import EliteBaby from "./game";
 import drawBackground from "./background";
+import Tile from "./tile";
 
 const canvas = document.getElementById("canvas");
 new EliteBaby(canvas);
 drawBackground();
+
+// new Tile(ctx)
 
