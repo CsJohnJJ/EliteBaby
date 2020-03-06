cf845a87a51bd4f995af503d82178f5e343a80a7
John Jiang <csjohnjj@gmail.com>
2020-03-06 02:09:45 -0500

    update candy flag for collision

--- src/flag.js
+++ src/flag.js
@@ -3,20 +3,27 @@ import GameObject from "./game_object";
 export default class Candy extends GameObject {
     constructor(ctx, x, y, vx, vy) {
         super(ctx, x, y, vx, vy);
-        this.width = 150;
-        this.height = 300;
+        this.width = 140;
+        this.height = 140;
     }
 
-    drawCandy() {
-        // this.ctx.clearRect(0, 0, 1000, 600);
-        const candyImage = new Image();
-        candyImage.src = "./src/images/food/candyFlag.png";
-        this.ctx.drawImage(candyImage, this.x, this.y, this.width, this.height)
-    }
+    // drawCandy() {
+    //     // this.ctx.clearRect(0, 0, 1000, 600);
+    //     const candyImage = new Image();
+    //     candyImage.src = "./src/images/food/candyFlag.png";
+    //     this.ctx.drawImage(candyImage, this.x, this.y, this.width, this.height)
+    // }
+
     drawCandyEnd(){
         const candyImage = new Image();
         candyImage.src = "./src/images/food/candyFlagTop.png";
-        this.ctx.drawImage(candyImage, this.x, this.y, this.width, this.height)
+        this.ctx.drawImage(candyImage, this.x, this.y, 150, 150)
+    }
+
+    drawCandyBottom() {
+        const candyImage = new Image();
+        candyImage.src = "./src/images/food/candyFlagBottom.png";
+        this.ctx.drawImage(candyImage, this.x + 75, this.y + 150, 20, 150)
     }
 
     update() {
