cf587679cdef43f8b0e3189ab89757e62760e20b
John Jiang <csjohnjj@gmail.com>
2020-03-06 15:59:32 -0500

    update deploy

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import EliteBaby from "./scripts/game";
 import drawBackground from "./scripts/background";
 import Tile from "./scripts/tile";
 
-const canvas = document.getElementById("canvas");
-new EliteBaby(canvas);
-drawBackground();
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("canvas");
+    new EliteBaby(canvas);
+    drawBackground();
+})
 
-// new Tile(ctx)
+
 
